e3ee67fad95ae43015dcb2778bf65a95ef937c4e
hasanfci@yahoo.com <hasanfci@yahoo.com>
2020-06-04 00:12:54 -0700

    STEP2 CK QBank all pages HTML

--- resources/views/notes/notes.blade.php
+++ resources/views/notes/notes.blade.php
@@ -6,8 +6,7 @@
 @section('content')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <div class="wrapper wrapper-content row animated fadeInRight ng-scope">
-    <div class="ibox ">
+     <div class="ibox ">
         <div class="ibox-content">
             <div class="pull-left input-group col-xs-12 col-sm-6 col-md-8 col-lg-5">
                 <input type="text" class="form-control ng-pristine ng-untouched ng-valid ng-valid-maxlength" ng-model="option.keyword" maxlength="100" ng-keyup="filterFunction()">
@@ -26,14 +25,12 @@
             <div class="clearfix"></div>
         </div>
     </div>
-    <div class="ibox ">
-        <div class="ibox-content inspinia-timeline">
+
+    <div class="ibox-content inspinia-timeline">
            
             <!-- ngIf: filteredData.length > 0 -->
             <!-- ngIf: filteredData.length == 0 --><div ng-if="filteredData.length == 0" class="ng-scope">No notes available</div><!-- end ngIf: filteredData.length == 0 -->
         </div>
-    </div>
-</div>
 </div>
 <!-- /.content-wrapper -->
 @endsection
--- resources/views/reset/reset.blade.php
+++ resources/views/reset/reset.blade.php
@@ -3,42 +3,30 @@
 @section('content')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <!-- Main row -->
-            <br/>
-            <div class="row">
-                <section class="col-lg-12 connectedSortable">
-                    <!-- DIRECT CHAT -->
-                    <div class="card direct-chat direct-chat-primary">
-                        <div class="card-header">
-                            <h3 class="card-title">Reset</h3>
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <div>
-                                <p style="padding: 10px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div>
-                        </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer">
-                           <h3 class="card-title"></h3>
-                        </div>
-                        <!-- /.card-footer-->
+    <div class="ibox-content">
+            <div class="col-sm-12 col-md-10">
+                <div class="alert alert-info">
+                    <ul class="ul-list">
+                        <li>Subscribers who have subscribed for 6 months or more can reset their test information <b>Only Once</b> during the entire subscription.</li>
+                        <li>Reset of Test info will DELETE ALL data related to the subscription including notes, highlights, performance and all previous tests. Flash card data will not be deleted.</li>
+                        <li>Once the information is reset, the change is irreversible and the data is lost forever.</li>
+                    </ul>
+
+                </div>
+
+                <!-- ngIf: message == '' --><div ng-if="message == ''" class="ng-scope">
+                    <!-- ngIf: !isEligibleForReset --><div ng-if="!isEligibleForReset" class="alert alert-danger ng-scope">
+                        Your subscription does not qualify for a reset or subscription has already been reset.
+                    </div><!-- end ngIf: !isEligibleForReset -->
+
+                    <div>
+                        <!-- ngIf: isEligibleForReset -->
                     </div>
-                    <!--/.direct-chat -->
-                </section>
+                </div><!-- end ngIf: message == '' -->
+                <!-- ngIf: message.length > 0 -->
             </div>
-            <!-- /.row (main row) -->
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+            <div class="clearfix"></div>
+        </div>
 </div>
 <!-- /.content-wrapper -->
 @endsection
