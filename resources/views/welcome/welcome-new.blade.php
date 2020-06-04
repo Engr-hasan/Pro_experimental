6812a503c00ceb725c8aa0efce83046a9068f051
furiousnur <bashar.csediu@gmail.com>
2020-06-04 17:58:24 +0600

    welcome page

--- resources/views/welcome/welcome-new.blade.php
+++ resources/views/welcome/welcome-new.blade.php
@@ -15,31 +15,63 @@
                         <hr>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-6">
-                    <!-- Donut chart -->
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h3 class="card-title">
-                                <i class="far fa-chart-bar"></i>
-                                Donut Chart
-                            </h3>
+                <div class="row">
+                    <div class="col-md-6 col-lg-6">
+                        <div class="row">
+                            <div class="col-md-6 col-lg-6">
+                                <canvas id="pieChart1"></canvas>
+                            </div>
+                            <div class="col-md-6 col-lg-6">
+                                <div ng-if="!globalConstants.qbankIsSim" class="ng-scope">
+                                    <ul class="list-group ">
+                                        <li class="list-group-item">
+                                            <span class="badge badge-plain ng-binding">3367</span>
+                                            Total Questions
+                                        </li>
+                                        <li class="list-group-item ">
+                                            <span class="badge badge-plain ng-binding">235</span>
+                                            <span class="badge badge-correct ng-binding">7%</span>
+                                            Used Questions
+                                        </li>
+                                        <li class="list-group-item">
 
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
-                                </button>
+                                            <span class="badge badge-plain ng-binding">3132</span>
+                                            <span class="badge badge-incorrect ng-binding">93%</span>
+                                            Unused Questions
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                        <div class="card-body">
-                            <div id="donut-chart" style="height: 300px;"></div>
-                        </div>
-                        <!-- /.card-body-->
                     </div>
-                    <!-- /.card -->
-                </div>
-                <div class="col-md-6 col-lg-6">
+                    <div class="col-md-6 col-lg-6">
+                        <div class="row">
+                            <div class="col-md-6 col-lg-6">
+                                <canvas id="pieChart2"></canvas>
+                            </div>
+                            <div class="col-md-6 col-lg-6">
+                                <div ng-if="!globalConstants.qbankIsSim" class="ng-scope">
+                                    <ul class="list-group ">
+                                        <li class="list-group-item">
+                                            <span class="badge badge-plain ng-binding">3367</span>
+                                            Total Questions
+                                        </li>
+                                        <li class="list-group-item ">
+                                            <span class="badge badge-plain ng-binding">235</span>
+                                            <span class="badge badge-correct ng-binding">7%</span>
+                                            Used Questions
+                                        </li>
+                                        <li class="list-group-item">
 
+                                            <span class="badge badge-plain ng-binding">3132</span>
+                                            <span class="badge badge-incorrect ng-binding">93%</span>
+                                            Unused Questions
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -48,312 +80,96 @@
 @endsection
 
 @section('js')
-    <!-- jQuery -->
-    <script src="{{asset('dashboard')}}/plugins/jquery/jquery.min.js"></script>
-    <!-- jQuery UI -->
-    <script src="{{asset('dashboard')}}/plugins/jquery-ui/jquery-ui.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{asset('dashboard')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="{{asset('dashboard')}}/dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{asset('dashboard')}}/dist/js/demo.js"></script>
-    <!-- FLOT CHARTS -->
-    <script src="{{asset('dashboard')}}/plugins/flot/jquery.flot.js"></script>
-    <!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
-    <script src="{{asset('dashboard')}}/plugins/flot-old/jquery.flot.resize.min.js"></script>
-    <!-- FLOT PIE PLUGIN - also used to draw donut charts -->
-    <script src="{{asset('dashboard')}}/plugins/flot-old/jquery.flot.pie.min.js"></script>
-
-    {{--<!-- FLOT PIE PLUGIN - also used to draw donut charts -->
-    <script src="{{asset('dashboard/plugins/flot/jquery.flot.js')}}"></script>
-    <script src="{{asset('dashboard/plugins/flot-old/jquery.flot.pie.min.js')}}"></script>--}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script>
-        $(function () {
-            /*
-             * Flot Interactive Chart
-             * -----------------------
-             */
-            // We use an inline data source in the example, usually data would
-            // be fetched from a server
-            var data        = [],
-                totalPoints = 100
-
-            function getRandomData() {
-
-                if (data.length > 0) {
-                    data = data.slice(1)
-                }
-
-                // Do a random walk
-                while (data.length < totalPoints) {
-
-                    var prev = data.length > 0 ? data[data.length - 1] : 50,
-                        y    = prev + Math.random() * 10 - 5
-
-                    if (y < 0) {
-                        y = 0
-                    } else if (y > 100) {
-                        y = 100
-                    }
-
-                    data.push(y)
-                }
-
-                // Zip the generated y values with the x values
-                var res = []
-                for (var i = 0; i < data.length; ++i) {
-                    res.push([i, data[i]])
-                }
-
-                return res
-            }
-
-            var interactive_plot = $.plot('#interactive', [
-                    {
-                        data: getRandomData(),
-                    }
-                ],
-                {
-                    grid: {
-                        borderColor: '#f3f3f3',
-                        borderWidth: 1,
-                        tickColor: '#f3f3f3'
-                    },
-                    series: {
-                        color: '#3c8dbc',
-                        lines: {
-                            lineWidth: 2,
-                            show: true,
-                            fill: true,
-                        },
-                    },
-                    yaxis: {
-                        min: 0,
-                        max: 100,
-                        show: true
-                    },
-                    xaxis: {
-                        show: true
-                    }
-                }
-            )
-
-            var updateInterval = 500 //Fetch data ever x milliseconds
-            var realtime       = 'on' //If == to on then fetch data every x seconds. else stop fetching
-            function update() {
-
-                interactive_plot.setData([getRandomData()])
-
-                // Since the axes don't change, we don't need to call plot.setupGrid()
-                interactive_plot.draw()
-                if (realtime === 'on') {
-                    setTimeout(update, updateInterval)
-                }
-            }
-
-            //INITIALIZE REALTIME DATA FETCHING
-            if (realtime === 'on') {
-                update()
-            }
-            //REALTIME TOGGLE
-            $('#realtime .btn').click(function () {
-                if ($(this).data('toggle') === 'on') {
-                    realtime = 'on'
-                }
-                else {
-                    realtime = 'off'
-                }
-                update()
-            })
-            /*
-             * END INTERACTIVE CHART
-             */
-
-
-            /*
-             * LINE CHART
-             * ----------
-             */
-            //LINE randomly generated data
-
-            var sin = [],
-                cos = []
-            for (var i = 0; i < 14; i += 0.5) {
-                sin.push([i, Math.sin(i)])
-                cos.push([i, Math.cos(i)])
-            }
-            var line_data1 = {
-                data : sin,
-                color: '#3c8dbc'
-            }
-            var line_data2 = {
-                data : cos,
-                color: '#00c0ef'
-            }
-            $.plot('#line-chart', [line_data1, line_data2], {
-                grid  : {
-                    hoverable  : true,
-                    borderColor: '#f3f3f3',
-                    borderWidth: 1,
-                    tickColor  : '#f3f3f3'
-                },
-                series: {
-                    shadowSize: 0,
-                    lines     : {
-                        show: true
-                    },
-                    points    : {
-                        show: true
-                    }
-                },
-                lines : {
-                    fill : false,
-                    color: ['#3c8dbc', '#f56954']
-                },
-                yaxis : {
-                    show: true
-                },
-                xaxis : {
-                    show: true
-                }
-            })
-            //Initialize tooltip on hover
-            $('<div class="tooltip-inner" id="line-chart-tooltip"></div>').css({
-                position: 'absolute',
-                display : 'none',
-                opacity : 0.8
-            }).appendTo('body')
-            $('#line-chart').bind('plothover', function (event, pos, item) {
-
-                if (item) {
-                    var x = item.datapoint[0].toFixed(2),
-                        y = item.datapoint[1].toFixed(2)
-
-                    $('#line-chart-tooltip').html(item.series.label + ' of ' + x + ' = ' + y)
-                        .css({
-                            top : item.pageY + 5,
-                            left: item.pageX + 5
-                        })
-                        .fadeIn(200)
-                } else {
-                    $('#line-chart-tooltip').hide()
-                }
-
-            })
-            /* END LINE CHART */
-
-            /*
-             * FULL WIDTH STATIC AREA CHART
-             * -----------------
-             */
-            var areaData = [[2, 88.0], [3, 93.3], [4, 102.0], [5, 108.5], [6, 115.7], [7, 115.6],
-                [8, 124.6], [9, 130.3], [10, 134.3], [11, 141.4], [12, 146.5], [13, 151.7], [14, 159.9],
-                [15, 165.4], [16, 167.8], [17, 168.7], [18, 169.5], [19, 168.0]]
-            $.plot('#area-chart', [areaData], {
-                grid  : {
-                    borderWidth: 0
-                },
-                series: {
-                    shadowSize: 0, // Drawing is faster without shadows
-                    color     : '#00c0ef',
-                    lines : {
-                        fill: true //Converts the line chart to area chart
-                    },
-                },
-                yaxis : {
-                    show: false
-                },
-                xaxis : {
-                    show: false
-                }
-            })
-
-            /* END AREA CHART */
-
-            /*
-             * BAR CHART
-             * ---------
-             */
-
-            var bar_data = {
-                data : [[1,10], [2,8], [3,4], [4,13], [5,17], [6,9]],
-                bars: { show: true }
-            }
-            $.plot('#bar-chart', [bar_data], {
-                grid  : {
-                    borderWidth: 1,
-                    borderColor: '#f3f3f3',
-                    tickColor  : '#f3f3f3'
-                },
-                series: {
-                    bars: {
-                        show: true, barWidth: 0.5, align: 'center',
-                    },
-                },
-                colors: ['#3c8dbc'],
-                xaxis : {
-                    ticks: [[1,'January'], [2,'February'], [3,'March'], [4,'April'], [5,'May'], [6,'June']]
-                }
-            })
-            /* END BAR CHART */
-
-            /*
-             * DONUT CHART
-             * -----------
-             */
-
-            var donutData = [
-                {
-                    label: 'Series2',
-                    data : 30,
-                    color: '#3c8dbc'
-                },
-                {
-                    label: 'Series3',
-                    data : 20,
-                    color: '#0073b7'
-                },
-                {
-                    label: 'Series4',
-                    data : 50,
-                    color: '#00c0ef'
-                }
-            ]
-            $.plot('#donut-chart', donutData, {
-                series: {
-                    pie: {
-                        show       : true,
-                        radius     : 1,
-                        innerRadius: 0.5,
-                        label      : {
-                            show     : true,
-                            radius   : 2 / 3,
-                            formatter: labelFormatter,
-                            threshold: 0.1
-                        }
-
-                    }
-                },
+        //pie
+        var ctxP = document.getElementById("pieChart1").getContext('2d');
+        var ctzp = document.getElementById("pieChart2").getContext('2d');
+        var myPieChart = new Chart(ctxP, {
+            type: 'pie',
+            data: {
+                labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+                datasets: [{
+                    data: [300, 50, 100, 40, 120],
+                    backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+                    hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+                }]
+            },
+            options: {
+                responsive: true,
                 legend: {
-                    show: false
+                    position: 'right',
+                    labels: {
+                        padding: 0,
+                        boxWidth: 20
+                    }
+                },
+                plugins: {
+                    datalabels: {
+                        formatter: (value, ctx) => {
+                            let sum = 0;
+                            let dataArr = ctx.chart.data.datasets[0].data;
+                            dataArr.map(data => {
+                                sum += data;
+                            });
+                            let percentage = (value * 100 / sum).toFixed(2) + "%";
+                            return percentage;
+                        },
+                        color: 'white',
+                        labels: {
+                            title: {
+                                font: {
+                                    size: '20'
+                                }
+                            }
+                        }
+                    }
                 }
-            })
-            /*
-             * END DONUT CHART
-             */
-
-        })
-
-        /*
-         * Custom Label formatter
-         * ----------------------
-         */
-        function labelFormatter(label, series) {
-            return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">'
-                + label
-                + '<br>'
-                + Math.round(series.percent) + '%</div>'
-        }
+            }
+        });
+        var myPieChart = new Chart(ctzp, {
+            type: 'pie',
+            data: {
+                labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+                datasets: [{
+                    data: [300, 50, 100, 40, 120],
+                    backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+                    hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+                }]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'right',
+                    labels: {
+                        padding: 0,
+                        boxWidth: 20
+                    }
+                },
+                plugins: {
+                    datalabels: {
+                        formatter: (value, ctx) => {
+                            let sum = 0;
+                            let dataArr = ctx.chart.data.datasets[0].data;
+                            dataArr.map(data => {
+                                sum += data;
+                            });
+                            let percentage = (value * 100 / sum).toFixed(2) + "%";
+                            return percentage;
+                        },
+                        color: 'white',
+                        labels: {
+                            title: {
+                                font: {
+                                    size: '20'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
     </script>
 @stop
