7faa5993d33a19143a70a0f3b8d02b291b93c0f1
furiousnur <bashar.csediu@gmail.com>
2020-06-20 17:00:22 +0600

    question mode remove from subject

--- database/seeds/SubjectSeeder.php
+++ database/seeds/SubjectSeeder.php
@@ -13,15 +13,12 @@ class SubjectSeeder extends Seeder
     {
         \DB::table('subjects')->insert([
             [
-                'question_mode_id' => 1,
                 'subject_name' => 'Medicine',
                 'status' => 1,
             ],[
-                'question_mode_id' => 1,
                 'subject_name' => 'Obstetrics & Gynecology',
                 'status' => 1,
             ],[
-                'question_mode_id' => 2,
                 'subject_name' => 'Psychiatry',
                 'status' => 1,
             ]
